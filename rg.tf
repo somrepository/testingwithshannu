f59447c817617cda2b6b0670c1b45e7263ae5230
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-12 05:33:46 -0400

    final

--- rg.tf
+++ rg.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "reporgf" {
+resource "azurerm_resource_group" "reporg" {
 name = "reporgn123"
 location = "East US" 
 }
 
 resource "azurerm_resource_group" "reporgfdfd" {
-name = "reporgn12ff
+name = "reporgn12ff"
 location = "East US" 
 }
--- storage.tf
+++ storage.tf
@@ -2,16 +2,7 @@ resource "azurerm_storage_account" "repstg" {
 name = "ropestg765"
 location = "west us"
 resouce_group_name = "reporg"
-account tier = "Standard"
-account_replication_type = "LRS"
-
-}
-
-resource "azurerm_storage_account" "repstg12" {
-name = "ropestg765"
-location = "East us"
-resouce_group_name = "reporg"
-account tier = "Standard"
+account_tier = "Standard"
 account_replication_type = "LRS"
 
 }

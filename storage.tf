7af20fbac981a00115af6218b6bad0c0cc90a2cf
shannsks <shannu.sks@gmail.com>
2025-04-12 08:26:01 +0000

    storageaccount added

---
+++ storage.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "repstg" {
+name = "ropestg765"
+location = "west us"
+resouce_group_name = "reporg"
+account tier = "Standard"
+account_replication_type = "LRS"
+
+}

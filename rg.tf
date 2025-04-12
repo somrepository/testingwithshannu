7076874c7d23fd4ffdbc14dcaef0595af20b7511
shannsks <shannu.sks@gmail.com>
2025-04-12 08:37:18 +0000

    added a rg

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "reporgf" {
 name = "reporgn123"
 location = "East US" 
 }
+
+resource "azurerm_resource_group" "reporgfdfd" {
+name = "reporgn12ff
+location = "East US" 
+}

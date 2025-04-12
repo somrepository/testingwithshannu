9a21d3177dd6a7990d2720f60f6a9d0e7de6b8d6
shannsks <shannu.sks@gmail.com>
2025-04-12 08:35:46 +0000

    RG and Storage

---
+++ rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "reporgf" {
+name = "reporgn123"
+location = "East US" 
+}
--- storage.tf
+++ storage.tf
@@ -6,3 +6,12 @@ account tier = "Standard"
 account_replication_type = "LRS"
 
 }
+
+resource "azurerm_storage_account" "repstg12" {
+name = "ropestg765"
+location = "East us"
+resouce_group_name = "reporg"
+account tier = "Standard"
+account_replication_type = "LRS"
+
+}

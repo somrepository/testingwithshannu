dff81ed26fd5e31ad569342167be52654ae132d5
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-12 08:14:02 +0000

    first

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.26.0
+    }
+}
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = ""
+} 
+
+resource "azurerm_resource_group" "reporg" {
+name = "reporgn"
+location = "East US" 
+}
